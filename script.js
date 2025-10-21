f9682b8b5396fc5121df8490b1249c9c17f6da5a
Entertained <72360637+Entertalned@users.noreply.github.com>
2025-10-21 19:02:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+let index = 0;
+
+function moveCarousel() {
+  index++;
+  if(index >= slides.length) index = 0;
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+// Auto slide every 3 seconds
+setInterval(moveCarousel, 3000);
 
